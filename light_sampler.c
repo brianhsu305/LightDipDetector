b095c5977eaf755a64de966c1d987dbac3e63c2a
Brian Hsu <brian@debian.brian>
2023-02-02 23:47:49 -0800

    added basic stuff

---
+++ light_sampler.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "sampler.h"
+
+
+int main(int argc, char* args[]) {
+    // Tasks:
+    // Analog to digital (A2D) reading (potentiometer and light sensor)
+    // Output on a 14-segment display using I2C
+    // UDP networking
+    // pthread threads and thread synchronization
+    // C-programming with malloc() / free()
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,12 @@
+OUTFILE = light_sampler
+OUTDIR = $(HOME)/cmpt433/public/myApps
+
+CROSS_COMPILE = arm-linux-gnueabihf-
+CC_C = $(CROSS_COMPILE)gcc
+CFLAGS = -Wall -g -std=c99 -D _POSIX_C_SOURCE=200809L -Werror -Wshadow
+
+app:
+	$(CC_C) $(CFLAGS) light_sampler.c -o $(OUTDIR)/$(OUTFILE)
+	
+clean: 
+	rm $(OUTDIR)/$(OUTFILE)
